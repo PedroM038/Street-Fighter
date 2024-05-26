1541d211d126838bb343a1082cac2c42aae0a5b8
PedroM038 <pedrohmarques640@gmail.com>
2024-05-26 18:10:22 -0300

    Implementação de um quadrado fixo nos extremos da tela

--- src/main.c
+++ src/main.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <allegro5/allegro5.h>
 #include <allegro5/allegro_font.h>
+#include <allegro5/allegro_primitives.h>
+
+#include "../include/square.h"
+
+#define XSCREEN 1280
+#define YSCREEN 720
 
 int main (void) {
     al_init();
+    al_init_primitives_addon();
     al_install_keyboard();
 
     ALLEGRO_TIMER* timer = al_create_timer(1.0 / 30.0);
@@ -15,6 +22,13 @@ int main (void) {
     al_register_event_source(queue, al_get_display_event_source(disp));
     al_register_event_source(queue, al_get_timer_event_source(timer));
 
+    square* player1 = squareCreate (100, 50, YSCREEN - 50, XSCREEN, YSCREEN);
+    if (!player1) return 1;
+    
+    square* player2 = squareCreate (100, XSCREEN - 50, YSCREEN - 50, XSCREEN, YSCREEN);
+    if (!player2) return 2;
+
+
     ALLEGRO_EVENT event;
     al_start_timer(timer);
 
@@ -24,16 +38,9 @@ int main (void) {
         al_wait_for_event(queue, &event);
 
         if (event.type == 30){
-            if (flag <= 15) {
-                al_clear_to_color(al_map_rgb(0,0,0));
-                flag++;
-            }
-            else {
-                al_clear_to_color(al_map_rgb(255,255,255));
-                flag++;
-                if (!(flag % 15)) flag = 0;
-            }
-
+            al_clear_to_color(al_map_rgb(0, 0, 0));																																			
+			al_draw_filled_rectangle(player1->x - player1->side/2, player1->y - player1->side/2, player1->x + player1->side/2, player1->y + player1->side/2, al_map_rgb(255, 0, 0));
+			al_draw_filled_rectangle(player2->x - player2->side/2, player2->y - player2->side/2, player2->x + player2->side/2, player2->y + player2->side/2, al_map_rgb(0, 0, 255));
             al_flip_display();
         }
 
@@ -44,6 +51,8 @@ int main (void) {
     al_destroy_display(disp);
     al_destroy_timer(timer);
     al_destroy_event_queue(queue);
+    squareDestroy(player1);
+    squareDestroy(player2);
 
     return 0;
 }
